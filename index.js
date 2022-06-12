b4dcbff9d82c7856a380636c9c1a5347abae5b39
narcpat <narcissearts@gmail.com>
2022-06-12 16:26:30 -0400

    added the Add Employee function

--- index.js
+++ index.js
@@ -96,12 +96,87 @@ function viewRole() {
 function viewEmployees() {
   db.promise()
     .query(
-      "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e2.first_name, ' ', e2.last_name) AS 'manager name' FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee AS e2 ON e2.manager_id = e2.id"
+      `SELECT 
+      employee.id, 
+      employee.first_name, 
+      employee.last_name, 
+      role.title, 
+      role.salary, 
+      department.name, 
+      CONCAT(e2.first_name, " " ,e2.last_name) AS 'manager name' FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee AS e2 ON employee.manager_id = e2.id`
     )
     .then(([response]) => {
       console.table(response);
       beginPrompts();
     });
+}
+
+function addEmployee() {
+  db.query("SELECT * FROM role", (err, data) => {
+    let all_role_ids = data.map(role => role.id);
+    console.log(all_role_ids);
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "first_name",
+          message: "Please enter employee's first name.",
+        },
+        {
+          type: "input",
+          name: "last_name",
+          message: "Please enter employee's last name.",
+        },
+        {
+          type: "list",
+          name: "role_id",
+          message: "Please select employee's role by ID.",
+          choices: all_role_ids,
+        },
+        {
+          type: "list",
+          name: "manager_id",
+          message: "Please select employee's manager by ID.",
+          choices: [1, 2, 3],
+        },
+      ])
+      .then(response => {
+        let result = db.query(`INSERT INTO employee SET ?`, {
+          first_name: response.first_name,
+          last_name: response.last_name,
+          role_id: response.role_id,
+          manager_id: response.manager_id,
+        });
+        beginPrompts();
+      });
+  });
+}
+
+function addRole() {
+  db.query("SELECT * FROM department", (err, data) => {
+    let all_department_ids = data.map(department => department.id);
+    console.log(all_department_ids);
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "title",
+          message: "Please enter the title for this role.",
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "Please enter the salary for this role.",
+        },
+      ])
+      .then(response => {
+        let result = db.query(`INSERT INTO role SET ?`, {
+          title: response.title,
+          salary: response.salary,
+        });
+        beginPrompts();
+      });
+  });
 }
 
 beginPrompts();
